038a20cde4613d3354f352940937113da30c3225
giuseppe <Giuseppearimatea10@gmail.com>
2020-01-26 18:53:55 +0100

    cerca utenti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 
   $(document).ready(function () {
+
+
+
+
+
     $('.icon-send').click(function () {
       sendMessage();
     });
-  });
+
 
 
 function sendMessage() {
@@ -34,3 +39,15 @@ function addZero(number) {
   }
   return number;
 }
+
+// funzione ricerca tra i nomi dei contatti
+  $('.search_click').click(function(){ // 1
+    var search = $('.cerca-chat').val();
+    var name = $('.persona p:first-child').text().toLowerCase();
+    var container = $("p.nomi:contains(" + search + ")");
+    console.log(container);
+    $('.elemento-contatto').addClass('active_name_off');
+    container.parents().removeClass('active_name_off');
+  });
+
+});
